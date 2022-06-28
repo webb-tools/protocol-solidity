5998a452381a0e8624681d8cdc33b8edc1742c25
nepoche <nepoche@yahoo.com>
2022-06-28 07:51:42 -0400

    Uncomment VBridge8Side deployment script for future use

--- scripts/evm/deployments/VBridge8Side.ts
+++ scripts/evm/deployments/VBridge8Side.ts
@@ -1,6 +1,6 @@
 import { VBridge } from '@webb-tools/vbridge';
-import { ERC20, GovernedTokenWrapper } from '@webb-tools/tokens';
-import { fetchComponentsFromFilePaths, getChainIdType } from '@webb-tools/utils';
+import { GovernedTokenWrapper } from '@webb-tools/tokens';
+import { fetchComponentsFromFilePaths } from '@webb-tools/utils';
 import { DeployerConfig } from '@webb-tools/interfaces';
 import path from 'path';
 import { ethers } from 'ethers';
@@ -13,9 +13,6 @@ import {
   chainIdTypeArbitrum,
   chainIdTypePolygon,
   chainIdTypeMoonbase,
-  chainIdTypeHermes,
-  chainIdTypeAthena,
-  chainIdTypeDemeter,
   walletRinkeby,
   walletGoerli,
   walletKovan,
@@ -24,9 +21,6 @@ import {
   walletArbitrum,
   walletPolygon,
   walletMoonbase,
-  walletHermes,
-  walletAthena,
-  walletDemeter
 } from '../ethersGovernorWallets';
 
 async function deploySignatureVBridge(
@@ -98,52 +92,26 @@ async function deploySignatureVBridge(
 
 async function run() {
   const deployers: DeployerConfig = {
-    wallets: {
-      // [chainIdTypeRinkeby]: walletRinkeby,
-      // [chainIdTypeGoerli]: walletGoerli,
-      // [chainIdTypeRopsten]: walletRopsten,
-      // [chainIdTypeKovan]: walletKovan,
-      // [chainIdTypeOptimism]: walletOptimism,
-      // [chainIdTypePolygon]: walletPolygon,
-      [chainIdTypeMoonbase]: walletMoonbase,
-      [chainIdTypeArbitrum]: walletArbitrum
-    },
-    gasLimits: {
-      // [chainIdTypeRinkeby]: '0x5B8D80',
-      // [chainIdTypeGoerli]: '0x5B8D80',
-      // [chainIdTypeRopsten]: '0x5B8D80',
-      // [chainIdTypeKovan]: '0x5B8D80',
-      // [chainIdTypeOptimism]: '0x5B8D80',
-      // [chainIdTypePolygon]: '0x5B8D80',
-      [chainIdTypeMoonbase]: '0x99999999999999999',
-      [chainIdTypeArbitrum]: '0x99999999'
-    }
+    [chainIdTypeRinkeby]: walletRinkeby,
+    [chainIdTypeGoerli]: walletGoerli,
+    [chainIdTypeRopsten]: walletRopsten,
+    [chainIdTypeKovan]: walletKovan,
+    [chainIdTypeOptimism]: walletOptimism,
+    [chainIdTypePolygon]: walletPolygon,
+    [chainIdTypeMoonbase]: walletMoonbase,
+    [chainIdTypeArbitrum]: walletArbitrum
   };
 
   const tokens: Record<number, string[]> = {
-    // [chainIdTypeRinkeby]: ['0', '0xc778417E063141139Fce010982780140Aa0cD5Ab'],
-    // [chainIdTypeGoerli]: ['0', '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'],
-    // [chainIdTypeRopsten]: ['0', '0xc778417E063141139Fce010982780140Aa0cD5Ab'],
-    // [chainIdTypeKovan]: ['0', '0xd0A1E359811322d97991E03f863a0C30C2cF029C'],
-    // [chainIdTypeOptimism]: ['0', '0xbC6F6b680bc61e30dB47721c6D1c5cde19C1300d'],
-    // [chainIdTypePolygon]: ['0', '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889'],
+    [chainIdTypeRinkeby]: ['0', '0xc778417E063141139Fce010982780140Aa0cD5Ab'],
+    [chainIdTypeGoerli]: ['0', '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'],
+    [chainIdTypeRopsten]: ['0', '0xc778417E063141139Fce010982780140Aa0cD5Ab'],
+    [chainIdTypeKovan]: ['0', '0xd0A1E359811322d97991E03f863a0C30C2cF029C'],
+    [chainIdTypeOptimism]: ['0', '0xbC6F6b680bc61e30dB47721c6D1c5cde19C1300d'],
+    [chainIdTypePolygon]: ['0', '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889'],
     [chainIdTypeMoonbase]: ['0xD909178CC99d318e4D46e7E66a972955859670E1'],
     [chainIdTypeArbitrum]: ['0', '0xEBbc3452Cc911591e4F18f3b36727Df45d6bd1f9']
   }
-
-  // const localDeployers: DeployerConfig = {
-  //   wallets: {
-  //     [chainIdTypeHermes]: walletHermes,
-  //     [chainIdTypeAthena]: walletAthena,
-  //     [chainIdTypeDemeter]: walletDemeter,
-  //   },
-  // };
-
-  // const localTokens: Record<number, string[]> = {
-  //   [chainIdTypeHermes]: ['0', '0xF2E246BB76DF876Cef8b38ae84130F4F55De395b'],
-  //   [chainIdTypeAthena]: ['0', '0xF2E246BB76DF876Cef8b38ae84130F4F55De395b'],
-  //   [chainIdTypeDemeter]: ['0', '0xF2E246BB76DF876Cef8b38ae84130F4F55De395b'],
-  // }
 
   const vbridge = await deploySignatureVBridge(
     tokens,
